8ecef5e35bc7b88159aec55833560c3631cef510
Alessio <alessioc.developer@gmail.com>
2024-03-14 09:14:20 +0100

    Added Bootstrap, Tailwind and DaisyUI

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const { default: daisyui } = require('daisyui');
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    '*.{html,js}',
+    './pages/**/*.{html,js}',
+    './resources/**/*.{html,js}'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require("daisyui")
+  ],
+}
+
