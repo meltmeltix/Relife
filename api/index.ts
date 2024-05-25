6c651ff7084185382053b1ab97428aa9b43f9198
Alessio <alessioc.developer@gmail.com>
2024-05-25 17:01:22 +0200

    Added readme images - Started setting up Express server both locally and through Vercel - Added temporary development page for color reference - Fixed missing Node dependency for index typescript

--- api/index.ts
+++ api/index.ts
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Hello world!"));
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.listen(port, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
