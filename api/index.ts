48f78a09aa2c9dff37f391b05f03bf56a1d28e1b
Alessio <alessioc.developer@gmail.com>
2024-05-27 22:03:54 +0200

    Discovered how EJS parameters work

--- api/index.ts
+++ api/index.ts
@@ -9,6 +9,7 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.get("/", (req, res) => res.render('pages/index'))
+app.get("/home", (req, res) => res.render('pages/feed/home'))
 
 app.listen(port, () => console.log("Server ready on port 3000."))
 
