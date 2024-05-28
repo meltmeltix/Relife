761e5e098fbefbc2c0179478ca211e42f00b9de8
Alessio <alessioc.developer@gmail.com>
2024-05-28 15:53:43 +0200

    Quick file management, added pagejs

--- api/index.ts
+++ api/index.ts
@@ -9,7 +9,7 @@ app.set('view engine', 'ejs')
 app.use(express.static(path.join(__dirname, '../public')))
 
 app.get("/", (req, res) => res.render('pages/index'))
-app.get("/home", (req, res) => res.render('pages/feed/home'))
+app.get("/home", (req, res) => res.render('pages/feed', {'content': 'HOME'}))
 
 app.listen(port, () => console.log("Server ready on port 3000."))
 
