071cb500b62fce03e6ccc5ecf95b23481ed362d3
Alessio <alessioc.developer@gmail.com>
2024-05-29 12:58:13 +0200

    Testing Vercel server - Started adding DB, its file and some of the DAOs

--- api/database.js
+++ api/database.js
@@ -1,7 +1,9 @@
 'use strict'
 
 const sqlite = require('sqlite')
-const dbSrc = './relife.db'
+const dbSrc = '../relife.db'
 const db = new sqlite.Database(dbSrc, (err) => {
     if (err) throw err
 })
+
+module.exports = db
