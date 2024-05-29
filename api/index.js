72fc7ecccaa0fa029fb7f0e8cd90c5f01c86de54
Alessio <alessioc.developer@gmail.com>
2024-05-29 23:38:53 +0200

    Added login strategy in server - Added bcrypt, express session and passport local - Finally got to populate the DB a bit - Moved things around again

--- api/index.js
+++ api/index.js
@@ -3,10 +3,25 @@ const path = require('path')
 const app = express()
 const port = 3000
 
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const session = require('express-session')
+
+const userDao = require('models/user-dao.js')
+
+// Views setup
 app.set('views', path.join(__dirname, '../views'))
 app.set('view engine', 'ejs')
-
 app.use(express.static(path.join(__dirname, '../public')))
+
+// Login strategy
+passport.use(new LocalStrategy(function verify(id, password, callback) {
+    userDao.getUser(id, password).then((user, check) => {
+        if (!user) return callback(null, false, { message: "Invalid identifier or password"})
+        if (!check) return callback(null, false, { message: "Invalid password" })
+        return callback(null, user)
+    })
+}))
 
 app.get("/", (req, res) => res.render('pages/index'))
 app.get("/home", (req, res) => res.render('pages/feed', {'content': 'HOME'}))
--- api/models/user-dao.js
+++ api/models/user-dao.js
@@ -1,9 +1,42 @@
 'use strict'
 
 const db = require('../database/db.js')
+const bcrypt = require('bcrypt');
+const mailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/
 
 exports.createUser = function() {
     return new Promise((resolve, rejects) => {
 
+    })
+}
+
+/**
+ * Given user data, execute select query to retrieve the user
+ * 
+ * @param {*} id            Can be either handle or mail
+ * @param {*} password      Secret to be checked
+ * @returns                 User and user existance, fullfilling the promise.
+ *                          Otherwise rejects with the respective error
+ */
+exports.getUser = function(id, password) {
+    return new Promise((resolve, rejects) => {
+        let isIdMail = false
+        let query = () => {
+            if (mailRegex.test(id)) return 'SELECT * FROM user WHERE id = ?'
+            else return 'SELECT * FROM user WHERE mail = ?'
+        }
+
+        db.get(query, [id], (err, row) => {
+            if (err) reject(err)
+            else if (row === undefined) resolve({error: 'User not found'})
+            else {
+                const user = {handle: row.handle, mail: row.mail}
+                let check = false
+
+                if (bcrypt.compareSync(password, row.password)) check = true
+
+                resolve({user, check})
+            }
+        })
     })
 }
