1651a0e27d62cbf82626fec4b0b337f049b9f0e8
Alessio <alessioc.developer@gmail.com>
2024-08-02 15:38:00 +0200

    Updated Feed to include guest access - Updated nav-bottom to now display login/signup buttons properly - Updated header to display dynamic page title - Added route to feed when reaching /explore

--- api/routes/feed.js
+++ api/routes/feed.js
@@ -4,7 +4,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/explore', function(req, res, next) {
-    res.render('feed', { userType: 'GUEST' })
+    res.render('feed', { userType: 'GUEST', title: 'Explore' })
 })
 
 module.exports = router
