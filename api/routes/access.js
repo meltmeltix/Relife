c3f61f6381cfa30498706f02e36f3ea613dee77b
Alessio <alessioc.developer@gmail.com>
2024-08-06 19:13:11 +0200

    First attempt to fix header issues

--- api/routes/access.js
+++ api/routes/access.js
@@ -9,12 +9,12 @@ router.get('/', function (req, res, next) {
 
 router.get('/signup', function (req, res, next) {
     if (req.isAuthenticated()) res.redirect('/home')
-    res.render('access', { accessType: "SIGNUP", title: "Sign Up", buttonText: "Next", message: null })
+    else res.render('access', { accessType: "SIGNUP", title: "Sign Up", buttonText: "Next", message: null })
 })
 
 router.get('/login', function (req, res, next) {
     if (req.isAuthenticated()) res.redirect('/home')
-    res.render('access', { accessType: "LOGIN", title: "Log In", buttonText: "Log In", message: null })
+    else res.render('access', { accessType: "LOGIN", title: "Log In", buttonText: "Log In", message: null })
 })
 
 module.exports = router
--- api/routes/feed.js
+++ api/routes/feed.js
@@ -5,22 +5,22 @@ const router = express.Router()
 
 router.get('/explore', function(req, res, next) {
     if(req.isAuthenticated()) res.redirect('/home')
-    res.render('feed', { sessionType: 'GUEST' })
+    else res.render('feed', { sessionType: 'GUEST' })
 })
 
 router.get('/home', function(req, res, next) {
     if(!req.isAuthenticated()) res.redirect('/')
-    res.render('feed', { sessionType: "USER" })
+    else res.render('feed', { sessionType: "USER" })
 })
 
 router.get('/search', function(req, res, next) {
     if(!req.isAuthenticated()) res.redirect('/')
-    res.render('feed', { sessionType: "USER" })
+    else res.render('feed', { sessionType: "USER" })
 })
 
 router.get('/profile', function(req, res, next) {
     if(!req.isAuthenticated()) res.redirect('/')
-    res.render('feed', { sessionType: "USER" })
+    else res.render('feed', { sessionType: "USER" })
 })
 
 module.exports = router
